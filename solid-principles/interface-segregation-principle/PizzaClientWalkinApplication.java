9dd60105be80e561cd25aa7d5e099e30a3913a0d
PRADEEP TIWARI <pradeep.tiwari555@gmail.com>
2023-02-19 18:37:19 +0530

    interface-segregation-principle

---
+++ solid-principles/interface-segregation-principle/PizzaClientWalkinApplication.java
@@ -0,0 +1,15 @@
+public class PizzaClientWalkinApplication implements PizzaByWalkinApplication {
+
+    @Override
+    public void orderPizzaByWalkin() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void acceptPaymentByCash() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ solid-principles/interface-segregation-principle/PizzaOnlineApplication.java
@@ -0,0 +1,4 @@
+public interface PizzaOnlineApplication {
+    public void orderPizzaOnline();
+    public void acceptPaymentOnline();
+}
