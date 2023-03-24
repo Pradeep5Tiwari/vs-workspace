4d3f8e158ec3148d92bec4364ef6e2d943c9a1b6
Pradeep Tiwari <pradeep.tiwari555@gmail.com>
2023-03-24 17:08:10 +0530

    singleton class modified

--- design-patterns/creational/singleton/Singleton.java
+++ design-patterns/creational/singleton/Singleton.java
@@ -1,5 +1,5 @@
 public class Singleton {
-    private static Singleton firstInstance = null; // Just one private static class type instance.
+    private static Singleton firstInstance; // Just one private static class type instance.
 
     private Singleton() { // private constructor so that we cannot create the object outside class via constructor. 
 
@@ -13,3 +13,45 @@ public class Singleton {
         return firstInstance; // since its class type we need to return it.
     }
 }
+
+
+/* 
+Question: Why singleton class is public?
+Answer: So that we can access the class and create its object in another class. If we declare this class as private then
+there is no scope of it outside the class itself.
+
+Question: Why "firstInstance" object is private? 
+Answer: "firstInstance" object of "Singleton" class is private so that you cannot access it directly.
+
+Question: If the Singleton class object "firstInstance" is private then how do we access it?
+Answer: In Singleton class we create the getInstance() to access the private object firstInstance.  
+
+Question: Why "firstInstance" object of "Singleton" class is static? 
+Answer: "firstInstance" object of "Singleton" class is static so that "firstInstance" object will be loaded and get memory 
+as soon as class "Singleton" will get loaded into memory at very first time. Though if you try to create object multiple time 
+then you won't be able to create the object. Only one time this "firstInstance" will be laoded into memory since its static.    
+
+Question: Why we don't create setter method in singleton class?
+Answer: So, that we cannot set the value and we can only get the value using getInstance() method.
+
+Question: Why the constructor is private?
+Answer: Constructor is private so that we cannot instantiate the class "Singleton" object using the "new" keyword.
+
+Question: What is eager loading and lazy loading in singleton class?
+Answer: 
+public class Sinleton {
+
+    private static Singleton firstInstance = new Singelton(); // Eager Loading
+}
+
+public class Singleton {
+    private static Singleton firstInstance = null;
+
+    public static Singleton getInstance() {
+        if(firstInstance == null) {
+            firstInstance = new Singleton();                // Lazy Loading
+        }
+    }
+} 
+
+*/
