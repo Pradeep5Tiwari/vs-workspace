dc91f04bb88eff9d6719723c41e0db4c8720ee7b
Pradeep Tiwari <pradeep.tiwari555@gmail.com>
2023-03-26 22:43:43 +0530

    proxy design pattern added

---
+++ design-patterns/structural/proxy design pattern/SubjectImplementaton.java
@@ -0,0 +1,8 @@
+// This is the implementation of subject
+public class SubjectImplementaton implements Subject {
+
+    @Override
+    public void myMethod() {
+        System.out.println("This is actual implementation of subject and rest all are proxies:  :D ");
+    }
+}
